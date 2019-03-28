5f8dfbda653bd7de1b559b52c5943429738593a1
RM <RRMoelker@users.noreply.github.com>
2019-03-28 16:16:38 +0100

    Extra fields disabled while awaiting approval

--- import/api/serializers.py
+++ import/api/serializers.py
@@ -26,7 +26,19 @@ class SpotDocumentSerializer(HALSerializer):
 class SpotGeojsonSerializer(GeoFeatureModelSerializer):
     class Meta(object):
         model = models.Spot
-        fields = '__all__'
+        # fields = '__all__' # TODO reactivate
+        fields = [
+            'documents',
+            'locatie_id',
+            'spot_type',
+            'description',
+            'point',
+            'stadsdeel',
+            'status',
+            'jaar_blackspotlijst',
+            'jaar_ongeval_quickscan',
+            'jaar_oplevering',
+        ]
         geo_field = 'point'
 
         # Detail url is constructed using location_id instead of pk,
@@ -41,7 +53,19 @@ class SpotSerializer(HALSerializer):
 
     class Meta(object):
         model = models.Spot
-        fields = '__all__'
+        # fields = '__all__' # TODO reactivate
+        fields = [
+            'documents',
+            'locatie_id',
+            'spot_type',
+            'description',
+            'point',
+            'stadsdeel',
+            'status',
+            'jaar_blackspotlijst',
+            'jaar_ongeval_quickscan',
+            'jaar_oplevering',
+        ]
 
         # Detail url is constructed using location_id instead of pk,
         # see: https://www.django-rest-framework.org/api-guide/serializers/#how-hyperlinked-views-are-determined # noqa: 501
